c158c5a3316c1f15871354e358b595136747198b
Daniel Weinberger <daniel.weinberger@yale.edu>
2018-06-29 21:32:05 -0400

    fix packages statement

--- main analysis components/synthetic_control_analysis.R
+++ main analysis components/synthetic_control_analysis.R
@@ -11,7 +11,7 @@
 
 source('synthetic_control_functions.R', local = TRUE)
 
-packages <- c('parallel', 'splines', 'lubridate','logistf', 'RcppRoll','pomp','lme4','stl', 'BoomSpikeSlab', 'ggplot2', 'reshape','dummies')
+packages <- c('parallel', 'splines', 'lubridate','logistf', 'RcppRoll','pomp','lme4', 'BoomSpikeSlab', 'ggplot2', 'reshape','dummies')
 packageHandler(packages, update_packages, install_packages)
 sapply(packages, library, quietly = TRUE, character.only = TRUE)
 
