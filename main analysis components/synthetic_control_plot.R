d79edd6dc97960bf2d1ab0a1fdbe635c0352f7ce
Daniel Weinberger <daniel.weinberger@yale.edu>
2018-07-11 19:03:27 -0400

    update plots to include stack estimates

--- main analysis components/synthetic_control_plot.R
+++ main analysis components/synthetic_control_plot.R
@@ -24,6 +24,10 @@ for (group in groups) {
 	pred_full_plot <-        plotPred(pred_quantiles_full[, , group], time_points, post_period, min_max, outcome_plot[, group], title = paste(group, 'Synthetic controls estimate'))
 	pred_time_plot <-        plotPred(pred_quantiles_time[, , group], time_points, post_period, min_max, outcome_plot[, group], title = paste(group, 'Interupted time series estimate'))
 	pred_pca_plot <-        plotPred(pred_quantiles_pca[, , group], time_points, post_period, min_max, outcome_plot[, group], title = paste(group, 'STL+PCA estimate'))
+	if(crossval){
+	  pred_stack_plot <-        plotPred(pred_quantiles_stack[, , group], time_points, post_period, min_max, outcome_plot[, group], title = paste(group, 'Stacked estimate'))
+ 
+	}
 	pred_sensitivity_plot <- plotPred(pred_quantiles_full[, , group], time_points, post_period, min_max, outcome_plot[, group], sensitivity_pred_quantiles = sensitivity_pred_quantiles[[group]], sensitivity_title = paste(group, 'Sensitivity Plots'), plot_sensitivity = TRUE)
 	
 	#matplot(pred_quantiles_full[, , 10], ylim=c(0,22000), type='l')	 ##Check
@@ -68,6 +72,21 @@ for (group in groups) {
 	        panel.background = element_blank()) +
 	  theme(legend.title = element_blank(), legend.position = c(0, 1), legend.justification = c(0, 1), legend.background = element_rect(colour = NA, fill = 'transparent'), plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 	
+	if(crossval){
+	  rr_roll_stack_plot <- ggplot(melt(as.data.frame(rr_roll_stack[, , group]), id.vars = NULL), mapping = aes_string(x = rep(time_points[(length(time_points) - nrow(rr_roll_stack[, , group]) + 1):length(time_points)], ncol(rr_roll_stack[, , group])), y = 'value', linetype = 'variable')) + 
+	    geom_line() + geom_hline(yintercept = 1, linetype = 4) +
+	    labs(x = 'Time', y = 'Rolling Rate Ratio') + 
+	    ggtitle(paste(group, 'Stacked Rolling Rate Ratio')) +
+	    coord_cartesian(ylim = min_max) +
+	    theme_bw() +
+	    theme(axis.line = element_line(colour = "black"),
+	          panel.grid.major = element_blank(),
+	          panel.grid.minor = element_blank(),
+	          panel.border = element_blank(),
+	          panel.background = element_blank()) +
+	    theme(legend.title = element_blank(), legend.position = c(0, 1), legend.justification = c(0, 1), legend.background = element_rect(colour = NA, fill = 'transparent'), plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+	  
+	}
 	#Plot cumulative sums
 	cumsum_prevented_plot <- ggplot(melt(as.data.frame(cumsum_prevented[, , group]), id.vars = NULL), mapping = aes_string(x = rep(time_points, ncol(cumsum_prevented[, , group])), y = 'value', linetype = 'variable')) + 
 		geom_line() + geom_hline(yintercept = 1, linetype = 4) +
@@ -80,16 +99,46 @@ for (group in groups) {
 	        panel.border = element_blank(),
 	        panel.background = element_blank()) +
 		theme(legend.title = element_blank(), legend.position = c(0, 1), legend.justification = c(0, 1), legend.background = element_rect(colour = NA, fill = 'transparent'), plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+	if( crossval){
+	  cumsum_prevented_stack_plot <- ggplot(melt(as.data.frame(cumsum_prevented_stack[, , group]), id.vars = NULL), mapping = aes_string(x = rep(time_points, ncol(cumsum_prevented_stack[, , group])), y = 'value', linetype = 'variable')) + 
+	    geom_line() + geom_hline(yintercept = 1, linetype = 4) +
+	    labs(x = 'Time', y = 'Cumulative Sum Prevented') + 
+	    ggtitle(paste(group, 'Cumulative Number of Cases Prevented (Stacked model)')) + 
+	    theme_bw() +
+	    theme(axis.line = element_line(colour = "black"),
+	          panel.grid.major = element_blank(),
+	          panel.grid.minor = element_blank(),
+	          panel.border = element_blank(),
+	          panel.background = element_blank()) +
+	    theme(legend.title = element_blank(), legend.position = c(0, 1), legend.justification = c(0, 1), legend.background = element_rect(colour = NA, fill = 'transparent'), plot.title = element_text(hjust = 0.5), panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+	  
+	}
 	
-	plot_list[[group]] <- list(covar_plot = covar_plot, 
+	
+if(crossval){	plot_list[[group]] <- list(covar_plot = covar_plot, 
+                                         
+                                         pred_stack_plot=pred_stack_plot,
 	                           	pred_full_plot = pred_full_plot, 
 															pred_time_plot = pred_time_plot, 
 															pred_pca_plot = pred_pca_plot, 
 															pred_sensitivity_plot = pred_sensitivity_plot, 
+															rr_roll_stack_plot = rr_roll_stack_plot, 
 															rr_roll_full_plot = rr_roll_full_plot, 
 															rr_roll_time_plot = rr_roll_time_plot, 
-															rr_roll_pca_plot = rr_roll_pca_plot, 
+															rr_roll_pca_plot = rr_roll_pca_plot,
+															cumsum_prevented_stack_plot=cumsum_prevented_stack_plot,
 															cumsum_prevented_plot = cumsum_prevented_plot)
+}else {
+  plot_list[[group]] <- list(covar_plot = covar_plot, 
+                             pred_full_plot = pred_full_plot, 
+                             pred_time_plot = pred_time_plot, 
+                             pred_pca_plot = pred_pca_plot, 
+                             pred_sensitivity_plot = pred_sensitivity_plot, 
+                             rr_roll_full_plot = rr_roll_full_plot, 
+                             rr_roll_time_plot = rr_roll_time_plot, 
+                             rr_roll_pca_plot = rr_roll_pca_plot, 
+                             cumsum_prevented_plot = cumsum_prevented_plot)
+}
 }
 
 for (group in groups) {
