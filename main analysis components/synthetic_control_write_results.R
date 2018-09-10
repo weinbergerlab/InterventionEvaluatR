b2f4a6cefaa94d01e47724f5ec4a61c2f41c81f1
Daniel Weinberger <stopaction13@gmail.com>
2018-09-10 15:54:39 -0400

    allow no sensitivity analysis

--- main analysis components/synthetic_control_write_results.R
+++ main analysis components/synthetic_control_write_results.R
@@ -19,7 +19,9 @@ write.csv(rr_roll_pca, paste(output_directory, country, '_rr_roll_pca.csv', sep 
 write.csv(stacking_weights.all, paste(output_directory, country, '_stacking_weights.csv', sep = ''))
 
 #Tables for rate ratios.
+if (!is.na(sensitivity_table_intervals)) {
 write.csv(sensitivity_table, paste(output_directory, country, '_sensitivity_table.csv', sep = ''))
 write.csv(sensitivity_table_intervals, paste(output_directory, country, '_sensitivity_table_intervals.csv', sep = ''))
 write.csv(rr_table, paste(output_directory, country, '_rr_table.csv', sep = ''))
 write.csv(rr_table_intervals, paste(output_directory, country, '_rr_table_intervals.csv', sep = ''))
+}
