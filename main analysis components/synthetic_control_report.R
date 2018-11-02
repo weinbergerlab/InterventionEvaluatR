408dbe8d575a54db45c7b651512c6ddd98a9c6f1
weinbergerlab <dweinber@gmail.com>
2018-11-02 11:47:30 -0400

    file path bug

--- main analysis components/synthetic_control_report.R
+++ main analysis components/synthetic_control_report.R
@@ -43,11 +43,11 @@ param_list <- list(update_packages = update_packages,
 							eval_period       = eval_period)
 run_pandoc <- rmarkdown::pandoc_available()
 if (run_pandoc) {
-	rmarkdown::render('synthetic_control_report.Rmd', output_file = 'Synthetic Control Report.html', output_dir = output_directory, params = param_list, envir = environment())	
-
+  rmarkdown::render('./main analysis components/synthetic_control_report.Rmd', output_file = 'Synthetic Control Report.html', output_dir = output_directory, params = param_list, envir = environment())	
+  
 } else {
-	knitr::knit('synthetic_control_report.Rmd', envir = environment())
-	markdown::markdownToHTML('synthetic_control_report.md', output = paste(output_directory, 'synthetic_control_report.html', sep = ''))
+  knitr::knit('./main analysis components/synthetic_control_report.Rmd', envir = environment())
+  markdown::markdownToHTML('synthetic_control_report.md', output = paste(output_directory, 'synthetic_control_report.html', sep = ''))
 	file.remove('synthetic_control_report.md')
 	unlink('figure/', recursive = TRUE)
 }
