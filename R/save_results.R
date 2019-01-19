324f6cdb99ecbcad148afa84dfe72b4ad9c04bf4
Ben Artin <ben@artins.org>
2019-01-18 22:05:00 -0500

    Migrated result writing to its own function inside the package

--- R/save_results.R
+++ R/save_results.R
@@ -1,6 +1,21 @@
-syncon.write_results <- function (syncon, output_directory, prefix) {
-  dir.create(output_directory, recursive = TRUE, showWarnings = FALSE)
-
+sc_save_results <- function (syncon, output_file) {
+  dir.create(dirname(output_file), recursive = TRUE, showWarnings = FALSE)
+  
+  saved = list(
+    version=1,
+    input=list(
+      country=syncon$country, 
+      data=syncon$input_data,
+      pre_priod=syncon$pre_period,
+      post_period=syncon$post_period,
+      eval_period=syncon$eval_period,
+      n_seasons=syncon$n_seasons, year_def=syncon$year_def, 
+      group_name=syncon$group_name, date_name=syncon$date_name, 
+      outcome_name=syncon$outcome_name, denom_name=syncon$denom_name
+    ),
+    results=syncon$results
+  )
+  
   prefix = paste0(output_directory, prefix)
 
   with(syncon$results, {
