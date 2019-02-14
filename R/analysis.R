f10d8d3af50cb539e0e8bd3dfe97c1463f445333
weinbergerlab <dweinber@gmail.com>
2019-02-13 21:52:51 -0500

    more comments

--- R/analysis.R
+++ R/analysis.R
@@ -1,42 +1,42 @@
 #' Initialize analysis
 #'
-#' @param country A character specifying the name fo the output. Should be a one-word label
-#' @param data Input dataframe
-#' @param pre_period_start TODO
-#' @param post_period_start TODO
-#' @param post_period_end TODO
-#' @param eval_period_start TODO
-#' @param eval_period_end TODO
-#' @param n_seasons TODO
-#' @param year_def TODO
-#' @param group_name TODO
-#' @param date_name TODO
-#' @param outcome_name TODO
-#' @param denom_name TODO
-#' @param sparse_threshold TODO
+#' @param country A one-word label for output (eg country or state name).
+#' @param data Input dataframe. There should be a row for each time point (e.g., month) and category (e.g., age group). There should be variables for the date, for the category (or a column of 1s if only 1 category), for the outcome variable (a count), for the denominator (or a column of 1s if no denominator), and columns for each control variable. 
+#' @param pre_period_start Date when analysis starts, YYYY-MM-01. defaults to first date in dataset.
+#' @param post_period_start Month when intervention introduced. YYY-MM-01
+#' @param post_period_end Date when analysis ends, YYYY-MM-01. defaults to first date in dataset. Defaults to end of evaluation period.
+#' @param eval_period_start First month of the period when the effect of intervention is evaluated. YYYY-MM-01. typically 12-24 months after post_period_start.
+#' @param eval_period_end Last month of the period when the effect of intervention is evaluated. YYYY-MM-01. 
+#' @param n_seasons How many observations per year? Defaults to 12 (monthly data) Change to 4 for quarterly
+#' @param year_def Should results be aggregated by calendar year ('cal_year': the default) or epidemiological year ('epi_year'; July-June)
+#' @param group_name Name of the stratification variable (e.g., age group). If only one age group present, add a column of 1s to the dataset
+#' @param date_name Name of the variable with the date for the time series
+#' @param outcome_name Name of the outcome (y) variable in the 'data' dataframe. Should be a count
+#' @param denom_name Name of the denominator variable in the 'data' dataframe. if there is no denominator, include a column of 1s.
+#' @param sparse_threshold Threshold for filtering out control variables based on sparsity (mean number of cases per time period). Defaults to 5. 
 #' @return Initialized analysis object, `analysis` as described below
 #'
-#' `analysis$country` as passed in in `country`
-#' `analysis$input_data` as passed in in `data`
-#' `analysis$n_seasons` as passed in in `n_seasons`
-#' `analysis$year_def` as passed in in `year_def`
-#' `analysis$pre_period` TODO
-#' `analysis$post_period` TODO
-#' `analysis$eval_period` TODO
-#' `analysis$start_date` TODO
-#' `analysis$intervention_date` TODO
-#' `analysis$end_date` TODO
-#' `analysis$group_name` as passed in in `group_name`
+#' `analysis$country` as passed to `country`
+#' `analysis$input_data` as passed to `data`
+#' `analysis$n_seasons` as passed to `n_seasons`
+#' `analysis$year_def` as passed to `year_def`
+#' `analysis$pre_period` Range of dates in the pre-intervention period
+#' `analysis$post_period` Range of dates in the post-intervention period
+#' `analysis$eval_period` Range of dates in the evaluation period
+#' `analysis$start_date` First date of the pre-intervention period
+#' `analysis$intervention_date` Last time point before the start of the post-period
+#' `analysis$end_date` Last date in the evaluation period
+#' `analysis$group_name` as passed to `group_name`
 #' `analysis$date_name` as passed in in `date_name`
 #' `analysis$outcome_name` as passed in in `outcome_name`
 #' `analysis$denom_name` as passed in in `denom_name`
-#' `analysis$time_points` TODO
-#' `analysis$groups` TODO
-#' `analysis$sparse_groups` TODO
+#' `analysis$time_points` Vector of time points in the dataset
+#' `analysis$groups` Vector of groups analyzed
+#' `analysis$sparse_groups` Vector indicating which groups were too sparse to analyze
 #' `analysis$model_size` TODO
 #' `analysis$covars` TODO
-#' `analysis$outcome` TODO
-#' `analysis$sparse_threshold` as passed in in `sparse_threshold`
+#' `analysis$outcome` as passeed to `outcome_name`
+#' `analysis$sparse_threshold` as passed to `sparse_threshold`
 #'
 #' @importFrom listenv listenv
 #' @export
@@ -511,7 +511,7 @@ syncon.crossval = function(analysis) {
             analysis$n_seasons,
             time_points = analysis$time_points,
             crossval.stage = TRUE,
-            var.select.on = analysis$.private$variants[[variant]]$var.select.on,
+            var.select.on = analysis$.private$variants[[variant]]$var.select.on
           )
       ),
       analysis$groups
